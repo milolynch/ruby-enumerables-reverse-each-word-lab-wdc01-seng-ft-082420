188b678941dc30f6cbdf1c3b08d7acd64d9f1b35
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-16 14:15:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
 def reverse_each_word (str)
   ary = str.split(" ")
   ary2 = []
+  rev_str = ""
+  index = 0
   ary.map do |word|
-    ary2 << word.reverse
+    ary2[index] = word.reverse
+    index +=1
   end
-  
+  ary2.collect do |dorw|
+    rev_str.concat(dorw)
+  end
+  rev_str
 end
